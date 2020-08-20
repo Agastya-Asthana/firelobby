85153078c92ed1a4536e93e93f6d6f3b9bab7313
Agastya Asthana <agastyaasthana@agastyas-air.home>
2020-08-19 20:30:39 -0500

    Continue Save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+
 import './App.css';
+import "bulma/css/bulma.css"
+
+import { FirebaseAppProvider, AuthCheck, useAuth, useFirestore, useFirestoreCollectionData } from "reactfire";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByxwJohw2Gqd_I57t8PmGvP5ZqoKH5x8U",
+  authDomain: "react-game-lobby.firebaseapp.com",
+  databaseURL: "https://react-game-lobby.firebaseio.com",
+  projectId: "react-game-lobby",
+  storageBucket: "react-game-lobby.appspot.com",
+  messagingSenderId: "11565285770",
+  appId: "1:11565285770:web:334779ae721dc0d84b4873",
+  measurementId: "G-RRHGL0NVCH"
+};
+
+function AuthButtons(){
+
+  const auth = useAuth();
+  /*const signIn = async () => {
+    await auth.signInWithPopup( new firebase.auth.GoogleAuthProvider() );
+  };
+  const signOut = async () => {
+    await auth.signOut();
+  };*/
+  return(
+    <AuthCheck
+      fallback={
+        <button id="SignInAuthButton" className="button is-dark signIn" onClick={
+          async (e) => {await auth.signInWithPopup( new firebase.auth.GoogleAuthProvider() )}
+        }>
+          Sign In <img id="GoogleLogo" width="32" src="https://img.icons8.com/color/48/000000/google-logo.png"/>
+        </button>
+      }
+    >
+
+      <button className="button is-outlined signOut" onClick={
+        async (e) => {await auth.signOut() }
+        }>Sign Out</button>
+    </AuthCheck>
+  );
+}
+
+function Navbar(){
+  return(
+    <nav className="navbar">
+      <div className="navbar-brand fireLobby">Fire Lobby <span role="img" aria-label="fire">🔥</span></div>
+      <div className="navbar-menu">
+        <div className="navbar-start"></div>
+        <div className="navbar-end">
+          <div className="navbar-items">
+            <div><AuthButtons /></div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function Lobby(){
+  const lobbyCollection = useFirestore().collection("lobby");
+  const lobby = useFirestoreCollectionData(lobbyCollection);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Navbar />
+        <div>Hello World! <span role="img" aria-label="globe">🌎</span></div>
+      </Suspense>
+    </FirebaseAppProvider>
+    
   );
 }
 
